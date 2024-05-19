7b925b1387fe05d704494a0ba2b2cd422ab52cf1
Loha <125302335+GabrielLoha-Dev@users.noreply.github.com>
2024-05-19 12:55:37 -0300

    esse site esta show de bola

---
+++ script.js
@@ -0,0 +1,7 @@
+function clickBtn() {
+    var textVar = document.getElementById('text')
+    var audioVar = document.getElementById('audio')
+
+    textVar.innerText = 'Meus Parabens HAHA'
+    audioVar.play()
+}
